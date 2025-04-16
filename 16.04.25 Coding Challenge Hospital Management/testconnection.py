63b6c4ae246f25ff30e20918f6f74eddae1fb2a6
Haley Pearl Caxmi T <129677772+caxmi@users.noreply.github.com>
2025-04-16 11:21:43 +0530

    Add files via upload

---
+++ 16.04.25 Coding Challenge Hospital Management/testconnection.py
@@ -0,0 +1,13 @@
+from util.db_conn_util import DBConnUtil
+
+def main():
+    # Test the database connection
+    conn = DBConnUtil.getConnection()
+
+    if conn:
+        print("Connection is active.")
+    else:
+        print("Connection failed.")
+
+if __name__ == '__main__':
+    main()
