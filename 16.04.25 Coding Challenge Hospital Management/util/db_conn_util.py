7a0df83cccbf5c145f9e7c6a1d1e3ab1f6b8c7e7
Haley Pearl Caxmi T <129677772+caxmi@users.noreply.github.com>
2025-04-16 11:28:40 +0530

    Delete 16.04.25 Coding Challenge Hospital Management directory

--- 16.04.25 Coding Challenge Hospital Management/util/db_conn_util.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-from util.db_property_util import DBPropertyUtil
-
-class DBConnUtil:
-    @staticmethod
-    def getConnection():
-        conn_str = DBPropertyUtil.getPropertyString('resources/db.properties')
-        return pyodbc.connect(conn_str)
